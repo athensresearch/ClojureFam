b4318b940cbd799496699898686ef566f5ed1955
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-04 13:06:22 -0400

    lazy-seq doc + exercise

--- src/athens/sandbox/seqs.clj
+++ src/athens/sandbox/seqs.clj
@@ -102,3 +102,64 @@
 (test #'key-sentence)
 ;; => :ok
 
+;; lazy-seq
+
+;; (lazy-seq & body)
+
+;; Takes a body of expressions that returns an `ISeq` or `nil`, and yields
+;; a Seqable object that will invoke the body only the first time `seq`
+;; is called, and will cache the result and return it on all subsequent
+;; `seq` calls.
+
+
+;; lazy-seq for reusing the result of expressions that create lazy sequences
+
+
+(def ls123 (lazy-seq [1 2 3]))
+;; => #'athens.sandbox.seqs/ls123
+
+ls123
+;; => (1 2 3)
+
+
+(time ls123)
+;; => "Elapsed time: 0.024633 msecs"
+
+(time (lazy-seq [1 2 3]))
+;; => "Elapsed time: 0.155445 msecs"
+
+;; Lazy sequences are especially useful for sequences that can go on for very long / infinitely
+
+(defn fib
+  ([]
+   (fib 1 1))
+  ([a b]
+   ;; Creates a lazy sequence of Fibonacci numbers
+   (lazy-seq (cons a (fib b (+ a b))))))
+
+(take 10 (fib))
+;; Returns the first 10 numbers of the Fibonacci sequence, try it out!
+
+
+
+;; Exercises
+
+
+(defn odd-numbers
+  {:test #(do
+            (assert (= (take 5  (odd-numbers))   '(1 3 5 7 9)))
+            (assert (= (take 10 (odd-numbers))   '(1 3 5 7 9 11 13 15 17 19)))
+            (assert (= (take 5  (odd-numbers 2)) '(3 5 7 9 11))))}
+  ([]
+   (odd-numbers 1))
+  ([a]
+   (if (= (mod a 2) 0)
+     (odd-numbers (inc a))
+     (lazy-seq (cons a (odd-numbers (+ a 2)))))))
+
+(test #'odd-numbers)
+;; => :ok
+
+
+
+

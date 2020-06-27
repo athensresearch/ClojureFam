482ab6e40b45c8956675e548cb4421f942bc3e16
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-26 22:11:54 -0400

    max profit koan

--- src/clojurefam/sandbox/bardia/tasks.clj
+++ src/clojurefam/sandbox/bardia/tasks.clj
@@ -23,3 +23,13 @@
                  (= false (solution [1 2 3 4]))
                  (= true  (solution [1 1 1 3 3 4 3 2 4 1])))
    :koan/description "Given an array of integers, find if the array contains any duplicates"})
+
+
+(defkoan ::max-profit
+  {:koan/problem '(and
+                   (= 5 (__ [7 1 5 3 6 4]))
+                   (= 0 (__ [7 6 4 3 1])))
+   :koan/check (fn [solution]
+                 (= 5 (solution [7 1 5 3 6 4]))
+                 (= 0 (solution [7 6 4 3 1])))
+   :koan/description "Say you have an array for which the ith element is the price of a given stock on day i. If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit. Note that you cannot sell a stock before you buy one."})

54505d7d4fa1beca83a209d5238beba999fd4586
Bardia Pourvakil <bardia.95.pourvakil@gmail.com>
2020-06-27 21:48:10 -0400

    anagram koan

--- src/clojurefam/sandbox/bardia/tasks.clj
+++ src/clojurefam/sandbox/bardia/tasks.clj
@@ -33,3 +33,13 @@
                  (= 5 (solution [7 1 5 3 6 4]))
                  (= 0 (solution [7 6 4 3 1])))
    :koan/description "Say you have an array for which the ith element is the price of a given stock on day i. If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit. Note that you cannot sell a stock before you buy one."})
+
+
+(defkoan ::is-anagram
+  {:koan/problem '(and
+                   (= true  (__ "anagram" "nagaram"))
+                   (= false (__ "rat" "car")))
+   :koan/check (fn [solution]
+                 (= true  (solution "anagram" "nagaram"))
+                 (= false (solution "rat" "car")))
+   :koan/description "Given two strings, determine if they are anagrams of each other"})
